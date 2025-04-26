fd7dc27a79fad975f8c2f59392ed4da7335e3d35
PLLima <74931964+PLLima@users.noreply.github.com>
2025-04-26 16:33:13 -0300

    chore: turn camera_view variables global

--- LAB02/Source_Code/src/main.cpp
+++ LAB02/Source_Code/src/main.cpp
@@ -110,6 +110,9 @@ bool g_LeftMouseButtonPressed = false;
 // usuário através do mouse (veja função CursorPosCallback()). A posição
 // efetiva da câmera é calculada dentro da função main(), dentro do loop de
 // renderização.
+glm::vec4 scene_origin_c     = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "c", centro da cena
+glm::vec4 camera_position_c  = glm::vec4(2.25f,2.25f,2.25f,1.0f); // Ponto "c", centro da câmera
+glm::vec4 camera_view_vector = scene_origin_c - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
 float g_CameraTheta = 0.0f; // Ângulo no plano ZX em relação ao eixo Z
 float g_CameraPhi = 0.0f;   // Ângulo em relação ao eixo Y
 float g_CameraDistance = 2.5f; // Distância da câmera para a origem
@@ -220,7 +223,6 @@ int main()
     glm::mat4 the_view;
 
     // Ficamos em um loop infinito, renderizando, até que o usuário feche a janela
-    glm::vec4 camera_position_c  = glm::vec4(2.25f,2.25f,2.25f,1.0f); // Ponto "c", centro da câmera
     while (!glfwWindowShouldClose(window))
     {
         // Aqui executamos as operações de renderização
@@ -258,8 +260,6 @@ int main()
         // Abaixo definimos as variáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
         // camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-        glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-        glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
         glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
